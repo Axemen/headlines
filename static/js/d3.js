b8763f3f9528508bae07eddde4fafdcd1c149678
Jonathan Randolph <jrandolph011@gmail.com>
2019-10-23 02:27:10 -0500

    rough draft of line chart moving onto making interactive and adding animations

--- static/js/d3.js
+++ static/js/d3.js
@@ -11,16 +11,56 @@ let chartMargin = {
 let chartWidth = svgWidth - chartMargin.left - chartMargin.right;
 let chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
 
-let svg = d3.select('body').append('svg')
+let svg = d3.select('#test-graph').append('svg')
     .attr('height', svgHeight)
     .attr('width', svgWidth)
 
 let chartGroup = svg.append('g')
-    .attr('transform', `translate(${chartMargin.left}, ${chartMargin.top})`);
+    .attr('transform', `translate(${chartMargin.left}, ${chartMargin.top})`)
+    .attr('height', chartHeight)
+    .attr('width', chartWidth);
 
-initBarGraph('us');
+d3.json(`/get_words/${'trump'}`).then(data => {
+    let parseTime = d3.timeParse('%Y')
+    data.forEach(d => d.year = parseTime(d.year))
 
-d3.select('#submit-words').on('click', d => {
-    let words = d3.select('#words-input').property('value')
-    updateBars(words, chartGroup);
-});
+    let xScale = d3.scaleTime()
+        .domain(d3.extent(data, d => d.year))
+        .range([0, chartWidth]);
+    
+    let yScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d=> d.count)])
+        .range([chartHeight, 0]);
+
+    let color = d3.scaleOrdinal(d3.schemeCategory10);
+
+    let line = d3.line()
+        .x(d => xScale(d.year))
+        .y(d => yScale(d.count));
+
+    chartGroup.append('path')
+        .datum(data)
+        .attr('class', 'line')
+        .style('fill', 'none')
+        .style('stroke', 'black')
+        .attr('d', line);
+
+    let xAxis = d3.axisBottom(xScale).ticks(11);
+    let yAxis = d3.axisLeft(yScale).ticks(10);
+
+    chartGroup.append('g')
+        .attr('class', 'xAxis')
+        .attr('transform', `translate(0, ${chartHeight})`)
+        .call(xAxis);
+
+    chartGroup.append('g')
+        .attr('class', 'yAxis')
+        .call(yAxis)
+        
+        
+})
+
+// d3.select('#submit-words').on('click', d => {
+//     let words = d3.select('#words-input').property('value')
+//     updateBars(words, chartGroup);
+// });
