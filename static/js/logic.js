aa016eebfcc5ed02c32f47df89c594f5e3d62507
Jonathan Randolph <jrandolph011@gmail.com>
2019-10-23 01:33:38 -0500

    implemented d3-bar on main page moving onto replace line chart with d3

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,6 +1,27 @@
 
 initGraph('us');
 
+
+let svgHeight = 450;
+let svgWidth = d3.select('#bar-graph').node().clientWidth;
+
+let chartMargin = {
+    top: 30,
+    right: 60,
+    bottom: 30,
+    left: 60
+};
+
+let chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+let chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+let svg = d3.select('#bar-graph').append('svg')
+    .attr('height', svgHeight)
+    .attr('width', svgWidth)
+
+let chartGroup = svg.append('g')
+    .attr('transform', `translate(${chartMargin.left}, ${chartMargin.top})`);
+
 initBarGraph('us');
 
 d3.select('#submit-words').on('click', d => {
@@ -15,6 +36,7 @@ d3.select('#remove-words').on('click', d => {
 
 d3.select('#submit-word-bar').on('click', d => {
     let word = d3.select('#words-input-bar').property('value')
-    updateBarGraph(word)
+    d3.select('#current-word-bar').html(`${word}`)
+    updateBars(word, chartGroup);
 })
 
